bef9d658413c346dc8a83295dea73bd6f8db77dd
PintoGamer64 <pintogamer64@gmail.com>
2023-05-19 21:30:11 -0500

    Drag And Drop Model
    Mover y ubicar el personaje donde quieras (pronto con autoguardado)

--- src/app/components/model/modelViewer.jsx
+++ src/app/components/model/modelViewer.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from "react"
+import { useCallback, useContext, useEffect, useRef, useState } from "react"
 
 // Hooks
 import useMicrophone from "../../hooks/AudioData";
@@ -13,6 +13,54 @@ import { fixRoute } from "../../controllers/fixRoute";
 
 export default function ModelViewer() {
 
+	const Viewer = useRef();
+	const Model = useRef();
+	const isClick = useRef(false);
+	const coords = useRef({
+		X: 0,
+		Y: 0,
+		lastX: 0,
+		lastY: 0
+	})
+	const executionMoveImg = useCallback(() => {
+		if (!Viewer.current || !Model.current) return;
+
+		const Vista = document.getElementById('ActualModel'), Modelo = document.getElementById('ActualModel');
+		//const Vista = Viewer.current, Modelo = Model.current;
+
+		const onMouseDown = event => {
+			isClick.current = true;
+			coords.current.X = event.clientX;
+			coords.current.Y = event.clientY;
+		}
+		const onMouseUp = () => {
+			isClick.current = false;
+			coords.current.lastX = Modelo.offsetLeft;
+			coords.current.lastY = Modelo.offsetTop;
+		}
+		const onMouseMove = event => {
+			if (!isClick.current) return;
+			Modelo.style.top = `${event.clientY - coords.current.Y + coords.current.lastY}px`
+			Modelo.style.left = `${event.clientX - coords.current.X + coords.current.lastX}px`
+		}
+
+		Modelo.addEventListener('mousedown', onMouseDown);
+		Modelo.addEventListener('mouseup', onMouseUp);
+		Vista.addEventListener('mousemove', onMouseMove);
+		Vista.addEventListener('mouseleave', onMouseUp);
+		
+		return () => {
+			Modelo.removeEventListener('mousedown', onMouseDown);
+			Modelo.removeEventListener('mouseup', onMouseUp);
+			Vista.removeEventListener('mousemove', onMouseMove);
+			Vista.removeEventListener('mouseleave', onMouseUp);
+		}
+	})
+
+	useEffect(() => {
+		executionMoveImg()
+	}, [])
+
 	const { stateModels } = useContext(Avatars);
 
 	const [Speak, setSpeak] = useState(false);
@@ -23,24 +71,18 @@ export default function ModelViewer() {
 	const { sensibility, capturerState } = AudioState;
 
 	useEffect(() => {
-		if (volume >= sensibility && capturerState ) {
+		if (volume >= sensibility && capturerState) {
 			setSpeak(true)
 			return;
 		}
 		setSpeak(false)
 	}, [volume])
-	
+
 	return (
-		<div id="ModelViewer">
-			<div id="ModelElements">
-				<div id="ModelSprites">
-					<img src={fixRoute(`${router}\\${select}\\${select}.png`)} alt="ModelSpritesManager" width={500} height={500} style={{
-						position: 'relative',
-						bottom: Speak ? 250 : 200,
-						animation: Speak ? "0.8s ease 0s infinite normal none running shake" : "none"
-					}} />
-				</div>
-			</div>
+		<div ref={Viewer} id="ModelViewer">
+			<img ref={Model} id="ActualModel" src={fixRoute(`${router}\\${select}\\${select}.png`)} alt="ModelSpritesManager" width={500} height={500} style={{
+				animation: Speak ? '0.8s ease 0s infinite normal none running shake' : 'none'
+			}} />
 		</div>
 	)
 }
